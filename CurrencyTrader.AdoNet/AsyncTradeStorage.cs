75d9d1b21b6a1dfdab1b9f4e77931ad80c96e1f3
Chad Parr <cparr2@css.edu>
2016-11-21 12:37:02 -0600

    partially done with assignmet not quite async

---
+++ CurrencyTrader.AdoNet/AsyncTradeStorage.cs
@@ -0,0 +1,29 @@
+﻿using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrader.AdoNet
+{
+    public class AsyncTradeStorage : ITradeStorage
+    {
+
+        private readonly ILogger logger;
+        private ITradeStorage SynchTradeStorage;
+
+        public AsyncTradeStorage(ILogger logger)
+        {
+            this.logger = logger;
+            SynchTradeStorage = new AdoNetTradeStorage(logger);
+        }
+
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            logger.LogInfo("Starting synch trade");
+            SynchTradeStorage.Persist(trades);
+
+        }
+    }
+}
